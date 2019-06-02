049c1f1d85df6ffaad52184400fd01f0914ce961
Ben Hoffman <bahoffman@users.noreply.github.com>
2019-06-02 18:36:16 +1200

    latest
     more form netlify

--- src/components/contactNetlify.js
+++ src/components/contactNetlify.js
@@ -2,45 +2,43 @@ import React from "react"
 // mport styled from "styled-components"
 
 const ContactForm = () => (
-  <form
-    name="contact"
-    method="POST"
-    netlify-honeypot="bot-field"
-    data-netlify="true"
-  >
-    <p class="hidden">
-      <label>
-        Don’t fill this out if you're human: <input name="bot-field" />
-      </label>
-    </p>
-    <p>
-      <label>
-        Your Name: <input type="text" name="name" />
-      </label>
-    </p>
-    <p>
-      <label>
-        Your Email: <input type="email" name="email" />
-      </label>
-    </p>
-    <p>
-      <label>
-        Your Role:{" "}
-        <select name="role[]" multiple>
-          <option value="leader">Leader</option>
-          <option value="follower">Follower</option>
-        </select>
-      </label>
-    </p>
-    <p>
-      <label>
-        Message: <textarea name="message" />
-      </label>
-    </p>
-    <p>
-      <button type="submit">Send</button>
-    </p>
-  </form>
+  <div>
+    <form
+      name="contact"
+      method="POST"
+      netlify-honeypot="bot-field"
+      data-netlify="true"
+    >
+      <input type="hidden" name="bot-field" />
+      <p>
+        <label>
+          Your Name: <input type="text" name="name" />
+        </label>
+      </p>
+      <p>
+        <label>
+          Your Email: <input type="email" name="email" />
+        </label>
+      </p>
+      <p>
+        <label>
+          Your Role:{" "}
+          <select name="role[]" multiple>
+            <option value="leader">Leader</option>
+            <option value="follower">Follower</option>
+          </select>
+        </label>
+      </p>
+      <p>
+        <label>
+          Message: <textarea name="message" />
+        </label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
+  </div>
 )
 
 export default ContactForm
